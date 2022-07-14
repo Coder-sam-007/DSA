ac7a4a4d54ee09d613febb93cd7ecd8e2720718a
Coder-sam-007 <padamzain178@gmail.com>
2022-07-14 14:27:30 +0530

    first commit

---
+++ array_insertion.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int myArray[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+  for (int i = 0; i < 10; i++)
+  {
+    printf("%d  ", myArray[i]);
+  }
+
+  int x = 50;
+  int position = 5;
+
+  printf("\nthe 10th element is %d\n", myArray[9]);
+
+  for (int i = 9; i >= position; i--)
+  {
+    myArray[i] = myArray[i - 1];
+  }
+
+  myArray[position - 1] = x;
+
+  for (int i = 0; i < 11; i++)
+  {
+    printf("%d  ", myArray[i]);
+  }
+
+  return 0;
+}
