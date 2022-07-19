ee99d26121f324f87c4d4de17d0f74fa38b451b8
Coder-sam-007 <padamzain178@gmail.com>
2022-07-19 14:14:10 +0530

    change

--- test.c
+++ test.c
@@ -3,7 +3,6 @@
 
 int main()
 {
-<<<<<<< HEAD
 
   int myArr[10] = {1, 2, 5, 4, 3, 3, 4, 5, 1, 2};
 
@@ -30,28 +29,6 @@ int main()
 
     printf(" the element %d is repeated %d times\n", myArr[j], count);
     count = 0;
-=======
-  int myArray[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-  for (int i = 0; i < 10; i++)
-  {
-    printf("%d  ", myArray[i]);
-  }
-
-  int x = 50;
-  int position = 5;
-
-  for (int i = 10; i >= position; i--)
-  {
-    myArray[i] = myArray[i - 1];
-  }
-
-  myArray[position - 1] = x;
-
-  for (int i = 0; i < 11; i++)
-  {
-    printf("%d  ", myArray[i]);
->>>>>>> 209ce6631ed69da9dab7e33246e4cdb1667464a8
   }
 
   return 0;
